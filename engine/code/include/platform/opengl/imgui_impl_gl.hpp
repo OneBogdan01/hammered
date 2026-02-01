fc266fccf7932bea7908571d7b503d3c058b063c
Bogdan Mocanu <bogdan.game.development@gmail.com>
2026-02-01 13:00:01 +0100

    refactor: start from scratch

--- engine/code/include/platform/opengl/imgui_impl_gl.hpp
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-namespace hm::external
-{
-void ImGuiInitializeOpenGL(SDL_Window* window, SDL_GLContext ctx);
-}
--- engine/code/source/external/glm_impl.cpp
+++
@@ -1,7 +0,0 @@
-﻿
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
-#include "glm/gtx/quaternion.hpp"
